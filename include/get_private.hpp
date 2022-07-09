f5822f7ef614304e847162bcd81fcbef3024f4e9
Tailing Yuan <yuantailing@gmail.com>
2022-07-10 01:30:57 +0800

    constexpr

--- include/get_private.hpp
+++ include/get_private.hpp
@@ -5,9 +5,9 @@ namespace { namespace get_private_detail_##func_name {  \
 using RetType = ret_type;  \
 template<typename T, T p>  \
 struct Tunnel {  \
-    friend inline RetType func_name() { return p; }  \
+    friend inline constexpr RetType func_name() { return p; }  \
 };  \
 template struct Tunnel<RetType, ret_val>;  \
-inline RetType func_name();  \
+inline constexpr RetType func_name();  \
 } }  \
 using get_private_detail_##func_name::func_name;

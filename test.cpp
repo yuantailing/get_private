9050ea8bc1b2c043214b9d4fc10545ca36eb717b
Tailing Yuan <yuantailing@gmail.com>
2022-07-10 01:27:10 +0800

    more complex examples

--- test.cpp
+++ test.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
+#include <string>
 
 #include "get_private.hpp"
 
 class A {
     int x;
-    double foo(double a) { return a * a; }
-    int foo(int a) { return a * a; }
+    template<typename T, typename U>
+    auto foo(T a, U b) -> decltype(a + b) { return a + b; }
     static double y;
-    static int bar(int a, int b) { return a + b; }
+    static int bar(int a, int b) { return a * b; }
+    static double bar(double a) { return a * a; }
 public:
     void check() const { std::cout << "x=" << x << " y=" << y << std::endl; }
 };
 
 double A::y{};
 
-DEFINE_GET_PRIVATE(int A::*, get_A_x, &A::x);
-DEFINE_GET_PRIVATE(double (A::*)(double), get_A_foo, &A::foo);
-DEFINE_GET_PRIVATE(double *, get_A_y, &A::y);
-DEFINE_GET_PRIVATE(int (*)(int, int), get_A_bar, &A::bar);
+DEFINE_GET_PRIVATE(int A::*, get_A_x, &A::x)
+DEFINE_GET_PRIVATE(double (A::*)(int, double), get_A_foo, &A::foo)
+DEFINE_GET_PRIVATE(std::string (A::*)(std::string, std::string), get_A_foo2, &A::foo)
+DEFINE_GET_PRIVATE(double *, get_A_y, &A::y)
+DEFINE_GET_PRIVATE(int (*)(int, int), get_A_bar, &A::bar)
+DEFINE_GET_PRIVATE(double (*)(double), get_A_bar2, &A::bar)
 
 int main() {
     A a;
     a.*get_A_x() = 123;
-    std::cout << (a.*get_A_foo())(3.14) << std::endl;
     *get_A_y() = 4.56;
-    std::cout << (*get_A_bar())(1, 2) << std::endl;
+    std::cout << (a.*get_A_foo())(1, 2) << std::endl;
+    std::cout << (a.*get_A_foo2())("Hello", "World") << std::endl;
+    std::cout << (*get_A_bar())(3, 4) << std::endl;
+    std::cout << (*get_A_bar2())(5.55) << std::endl;
     a.check();
 }

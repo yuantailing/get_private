b4fefb225cb62bd4def71937b380da5eaa09eec3
Tailing Yuan <yuantailing@gmail.com>
2022-07-10 23:27:25 +0800

    align

--- include/get_private.hpp
+++ include/get_private.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-#define DEFINE_GET_PRIVATE(ret_type, func_name, ret_val)  \
-namespace { namespace get_private_detail_##func_name {  \
-using RetType = ret_type;  \
-template<typename T, T p>  \
-struct Tunnel {  \
-    friend inline constexpr RetType func_name() { return p; }  \
-};  \
-template struct Tunnel<RetType, ret_val>;  \
-inline constexpr RetType func_name();  \
-} }  \
-using get_private_detail_##func_name::func_name;
+#define DEFINE_GET_PRIVATE(ret_type, func_name, ret_val)           \
+    namespace { namespace get_private_detail_##func_name {         \
+    using RetType = ret_type;                                      \
+    template<typename T, T p>                                      \
+    struct Tunnel {                                                \
+        friend inline constexpr RetType func_name() { return p; }  \
+    };                                                             \
+    template struct Tunnel<RetType, ret_val>;                      \
+    inline constexpr RetType func_name();                          \
+    } }                                                            \
+    using get_private_detail_##func_name::func_name;

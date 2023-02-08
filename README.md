c1f2739fa2f39be702686558f9662238bd74ee9d
Tailing Yuan <yuantailing@gmail.com>
2023-02-08 22:27:50 +0800

    update README.md

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 Get private members of a C++ class.
 
 ```cpp
+#include <iostream>
 #include "get_private.hpp"
 
 class A {
     static int x;
     double y;
+    void print(std::ostream &os) const { os << x << " " << y << std::endl; }
 };
 
 int A::x = 0;
@@ -17,6 +19,9 @@ DEFINE_GET_PRIVATE(int &, get_A_x,           A::x)
 //                 double A::*  get_A_y() { return &A::y; }
 DEFINE_GET_PRIVATE(double A::*, get_A_y,           &A::y)
 
+//                 void (A::*)(std::ostream &) const, get_A_print() { return &A::print; }
+DEFINE_GET_PRIVATE(void (A::*)(std::ostream &) const, get_A_print,           &A::print)
+
 int main() {
     // A::x = 123;  // => Compilation error. Cannot access private members.
     get_A_x() = 123;
@@ -24,6 +29,9 @@ int main() {
     A a;
     // a.y = 4.56;
     a.*get_A_y() = 4.56;
+
+    // a.print(std::cout);
+    (a.*get_A_print())(std::cout);
 }
 ```
 
--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@
 class A {
     int x;
     template<typename T, typename U>
-    auto foo(T a, U b) -> decltype(a + b) { return a + b; }
+    auto foo(T a, U b) const -> decltype(a + b) { return a + b; }
     static double y;
     static int bar(int a, int b) { return a * b; }
     static double bar(double a) { return a * a; }
@@ -17,8 +17,8 @@ public:
 double A::y{};
 
 DEFINE_GET_PRIVATE(int A::*, get_A_x, &A::x)
-DEFINE_GET_PRIVATE(double (A::*)(int, double), get_A_foo, &A::foo)
-DEFINE_GET_PRIVATE(std::string (A::*)(std::string, std::string), get_A_foo2, &A::foo)
+DEFINE_GET_PRIVATE(double (A::*)(int, double) const, get_A_foo, &A::foo)
+DEFINE_GET_PRIVATE(std::string (A::*)(std::string, std::string) const, get_A_foo2, &A::foo)
 DEFINE_GET_PRIVATE(double &, get_A_y, A::y)
 DEFINE_GET_PRIVATE(int (*)(int, int), get_A_bar, &A::bar)
 DEFINE_GET_PRIVATE(double (*)(double), get_A_bar2, &A::bar)
@@ -27,9 +27,9 @@ int main() {
     A a;
     a.*get_A_x() = 123;
     get_A_y() = 4.56;
-    std::cout << (a.*get_A_foo())(1, 2) << std::endl;
+    std::cout << (a.*get_A_foo())(1, 2.34) << std::endl;
     std::cout << (a.*get_A_foo2())("Hello", "World") << std::endl;
-    std::cout << (*get_A_bar())(3, 4) << std::endl;
-    std::cout << (*get_A_bar2())(5.55) << std::endl;
+    std::cout << (*get_A_bar())(5, 6) << std::endl;
+    std::cout << (*get_A_bar2())(7.89) << std::endl;
     a.check();
 }
